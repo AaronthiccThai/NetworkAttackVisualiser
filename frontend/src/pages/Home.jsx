602464628b8d62791ff90e53e3ed6a9adc7167fb
Aaron Thai <howaaronic1@gmail.com>
2025-06-30 14:12:23 +1000

    updated home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,24 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="p-4 text-center">
-      <h1 className="text-3xl font-bold mb-4">Network Packet Visualiser</h1>
-      <Link to="/synflood" className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-        Go to SYN Flood Simulation
-      </Link>
-      
+    <div className="relative min-h-screen bg-white p-4">
+      <div className="absolute top-4 right-4 text-sm text-black font-semibold">
+        Aaron Thai - z5481487, COMP6841 25T2
+      </div>
+      <div className="text-center mt-24 text-black">
+        <h1 className="text-3xl font-bold mb-4">Network Packet Visualiser</h1>
+        <h2 className="mb-4">This is a simple network packet visualiser that showcases what some network attacks look like.</h2>
+        <h2 className="mb-4">
+          Network attacks are important to understand as they are significant threat to individuals and organisations.
+        </h2>
+        <h2> They can cause potential financial losses, data breaches, and reputational damage.</h2>
+        <Link
+          to="/synflood"
+          className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+        >
+          Go to SYN Flood Simulation
+        </Link>
+      </div>
     </div>
   );
 };
